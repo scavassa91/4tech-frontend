8fdacbef530dda6aba158f0ff30240ad5b469c47
scavassa91 <dan.scavassa@gmail.com>
2020-01-28 13:55:31 -0300

    Fifth commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { logout } from '../../services/auth';
+
 import { AppBar, Container, Toolbar, Typography, IconButton } from '@material-ui/core';
 import { ExitToApp } from '@material-ui/icons';
 
 import './Header.css';
 
 const Header = () => {
+    const history = useHistory();
 
     const onLogout = () => {
-
+        logout();
+        history.push('/');
     };
 
     return (
